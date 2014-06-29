19c857a78107a9e5a1affa89ef7eaf0b6e0e6ac6
Mark Norman Francis <norm@cackhanded.net>
2014-06-29 09:21:53 +0100

    Manage git configuration through `homedir`

--- modules/people/manifests/norm.pp
+++ modules/people/manifests/norm.pp
@@ -1,6 +1,5 @@
 class people::norm {
     include norm::software
     include norm::osx
-    include norm::git_config
     include norm::settings
 }
--- modules/people/manifests/norm/git_config.pp
+++
@@ -1,4 +0,0 @@
-class people::norm::git_config {
-    git::config::global { 'alias.s': value => 'status -bs' }
-    git::config::global { 'push.default': value => 'simple' }
-}
