e57cea323a3f37b26b1883f1fbc9480bca110b30
Mark Norman Francis <norm@cackhanded.net>
2014-06-29 08:41:13 +0100

    Can turn off the Dashboard

---
+++ modules/turn-off-dashboard/manifests/init.pp
@@ -0,0 +1,12 @@
+# Turns off the OSX Dashboard feature
+class turn-off-dashboard {
+    include osx::dock
+
+    boxen::osx_defaults { 'Disables Dashboard':
+        user   => $::boxen_user,
+        domain => 'com.apple.dashboard',
+        key    => 'mcx-disabled',
+        value  => true,
+        notify => Exec['killall Dock'];
+    }
+}
