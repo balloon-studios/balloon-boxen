c94198d6812b00a097a3838eca436e34915af7bf
Mark Norman Francis <norm@cackhanded.net>
2014-07-23 10:26:39 +0100

    [norm] sort

--- modules/people/manifests/norm/osx.pp
+++ modules/people/manifests/norm/osx.pp
@@ -8,8 +8,8 @@ class people::norm::osx {
     include osx::dock::2d
     include osx::dock::autohide
 
-    include osx::finder::show_all_on_desktop
     include osx::finder::empty_trash_securely
+    include osx::finder::show_all_on_desktop
     include osx::finder::unhide_library
 
     include osx::universal_access::ctrl_mod_zoom
