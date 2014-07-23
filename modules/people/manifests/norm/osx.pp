ef84961b80412120cbf338ed9a6e50012336150f
Mark Norman Francis <norm@cackhanded.net>
2014-07-23 10:26:30 +0100

    [norm] likes standard function keys

--- modules/people/manifests/norm/osx.pp
+++ modules/people/manifests/norm/osx.pp
@@ -1,5 +1,6 @@
 class people::norm::osx {
     include osx::global::enable_keyboard_control_access
+    include osx::global::enable_standard_function_keys
     include osx::global::expand_print_dialog
     include osx::global::expand_save_dialog
 
