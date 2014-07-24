f295a080d7239fd73244a9a16dae9713372c71fb
Mark Norman Francis <norm@cackhanded.net>
2014-07-24 11:09:42 +0100

    [norm] likes alphabetical ordering

--- modules/people/manifests/norm/settings.pp
+++ modules/people/manifests/norm/settings.pp
@@ -1,5 +1,5 @@
 class people::norm::settings {
-    repo::personal::install { 'osx_shortcuts': }
     repo::personal::install { 'bash-composition': }
     repo::personal::install { 'homedir': }
+    repo::personal::install { 'osx_shortcuts': }
 }
