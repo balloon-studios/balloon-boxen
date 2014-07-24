16d71d491c9d5f9c75ec19f3cf9f247399b521de
Meri Williams <meri@chromerose.co.uk>
2014-07-24 15:23:12 +0100

    [geekmanager] including virtualbox for geekmanager

--- modules/people/manifests/geekmanager.pp
+++ modules/people/manifests/geekmanager.pp
@@ -1,4 +1,9 @@
 class people::geekmanager {
     include geekmanager::software
     include geekmanager::osx
+    
+    include projects::development
+    include projects::infrastructure
+
+    include virtualbox
 }
