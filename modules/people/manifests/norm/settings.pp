b3ee11aec9ee9695ac2015dca913cf1170dde79c
Mark Norman Francis <norm@cackhanded.net>
2014-07-24 11:10:27 +0100

    [norm] has more settings and scripts to install

--- modules/people/manifests/norm/settings.pp
+++ modules/people/manifests/norm/settings.pp
@@ -1,5 +1,7 @@
 class people::norm::settings {
     repo::personal::install { 'bash-composition': }
     repo::personal::install { 'homedir': }
+    repo::personal::install { 'osx_scripts': }
+    repo::personal::install { 'osx_settings': }
     repo::personal::install { 'osx_shortcuts': }
 }
