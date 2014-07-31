ca56f4b965314ef3bdfe5004189a6631381c9a78
Mark Norman Francis <norm@cackhanded.net>
2014-07-31 14:46:15 +0100

    [norm] works with slackbots

--- modules/people/manifests/norm.pp
+++ modules/people/manifests/norm.pp
@@ -3,6 +3,9 @@ class people::norm {
     include norm::osx
     include norm::settings
 
+    repo::balloon { 'quotebot': }
+    repo::balloon { 'slack-echo-chamber': }
+
     include projects::development
     include projects::infrastructure
 }
