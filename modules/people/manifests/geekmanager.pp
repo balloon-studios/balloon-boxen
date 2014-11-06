2f928501f38ac06a0aa19ec459cee0da98393e7f
Meri Williams <meri@chromerose.co.uk>
2014-11-06 16:21:31 +0000

    Updates manifests to include new projects
    Apologies in advance if updating another person’s manifest is Not Good
    Form — was trying to be helpful.

--- modules/people/manifests/geekmanager.pp
+++ modules/people/manifests/geekmanager.pp
@@ -2,8 +2,10 @@ class people::geekmanager {
     include geekmanager::software
     include geekmanager::osx
     
+    include projects::balloon-studios
     include projects::development
     include projects::infrastructure
+    include projects::marks-and-spencer
 
     include virtualbox
 }
