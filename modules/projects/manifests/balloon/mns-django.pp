09c87006c8dc0d4fad386143fb6e4755069ad1a6
Meri Williams <meri@chromerose.co.uk>
2014-11-06 16:18:43 +0000

    Moves all M&S stuff into marks-and-spencer project
    And removes mns-django, but replicates the content in the new
    marks-and-spencer project.

--- modules/projects/manifests/balloon/mns-django.pp
+++
@@ -1,6 +0,0 @@
-class projects::balloon::mns-django {
-    repo::balloon::mns { 'mns-infra': }
-    repo::balloon::mns { 'mns-django-templates': }
-    repo::balloon::mns { 'mns-multi-search': }
-    repo::balloon::mns { 'mns-api-access': }
-}
