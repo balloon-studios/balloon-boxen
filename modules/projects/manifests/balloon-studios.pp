956b22cd09de298b3e20acb1e311225e4aa294f3
Meri Williams <meri@chromerose.co.uk>
2014-11-06 16:19:00 +0000

    Creates balloon-studios project
    For all our company stuff

---
+++ modules/projects/manifests/balloon-studios.pp
@@ -0,0 +1,10 @@
+class projects::balloon-studios {
+    
+    repo::balloon { 'quotebot': }
+    repo::balloon { 'slack-echo-chamber': }
+    repo::balloon { 'balloon-boxen': }
+    repo::balloon { 'balloon-utils': }
+    repo::balloon { 'srsbsns': }
+    repo::balloon { 'balloonstudios.com': }
+
+}
